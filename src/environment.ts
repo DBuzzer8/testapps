c4cbb20af55ff3fd1eb97c1efe9cf91d33ef1949
robin goh <dealsbuzzer8@gmail.com>
2017-07-05 13:27:15 +0800

    0.1

---
+++ src/environment.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyD2Lkz-3kgCmaOYnvHVQtNE8s1W0Sp9zxY',
+    authDomain: 'cppp1-72cc4.firebaseapp.com',
+    databaseURL: 'https://cppp1-72cc4.firebaseio.com',
+    projectId: 'cppp1-72cc4',
+    storageBucket: 'cppp1-72cc4.appspot.com',
+    messagingSenderId: '103404407874'
+  }
